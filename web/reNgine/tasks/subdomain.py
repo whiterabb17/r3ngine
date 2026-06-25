e9dd98828b4357baf7f5f7b2e6acb826bf797d99
s1l3nt78 <90134688+whiterabb17@users.noreply.github.com>
2026-06-25 22:17:29 +0200

    fix(tasks): add missing run_command_with_retry and save_subdomain_metadata imports to subdomain.py

--- web/reNgine/tasks/subdomain.py
+++ web/reNgine/tasks/subdomain.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from reNgine.common_func import *
 from reNgine.definitions import *
 from reNgine.utils.opsec import OpSecManager, ProxychainsWrapper, get_opsec_manager
-from reNgine.utils.task import run_command, stream_command, save_subdomain, save_endpoint
+from reNgine.utils.task import run_command, run_command_with_retry, stream_command, save_subdomain, save_endpoint, save_subdomain_metadata
 from reNgine.tasks.persistence import create_scan_activity
 from startScan.models import *
 from targetApp.models import Domain
