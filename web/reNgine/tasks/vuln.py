6e7a30d88927f814f714d17777439bb642a236c2
s1l3nt78 <90134688+whiterabb17@users.noreply.github.com>
2026-07-07 19:40:10 +0200

    feat(smugglex): enhance smugglex checks

--- web/reNgine/tasks/vuln.py
+++ web/reNgine/tasks/vuln.py
@@ -1305,10 +1305,17 @@ def smugglex_scan(self, urls=[], ctx={}, description=None):
 		logger.warning('smugglex: no endpoints to scan, skipping.')
 		return
 
+	proxy = get_random_proxy()
 	output_json = f"{self.results_dir}/smugglex_output.json"
-	cmd = f"cat {input_path} | smugglex --json -o {output_json}"
+	# add proxy to the command
+	if proxy:
+		cmd = f"cat {input_path} | smugglex --json -o {output_json} --proxy {proxy}"
+	else:
+		cmd = f"cat {input_path} | smugglex --json -o {output_json}"
+	cmd += f" -c cl-te,te-cl,te-te,h2c,h2,cl-edge,h2-downgrade"
+	cmd += f" --fuzz"
 	run_command(cmd, shell=True, scan_id=self.scan_id, activity_id=self.activity_id)
-	
+
 	if os.path.exists(output_json):
 		try:
 			with open(output_json, 'r') as f:
